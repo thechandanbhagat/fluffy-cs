598ef5414ac11e6de8ae2ff554cd2ff2deb603df
Chandan Bhagat <chandan.bhagat@outlook.com>
2023-02-09 11:26:40 +0000

    generate hash as byte for string

--- CS/Extensions/StringExtension.cs
+++ CS/Extensions/StringExtension.cs
@@ -11,6 +11,20 @@ namespace CS.Extensions
         {
             return string.IsNullOrEmpty(str) || str.Length < 2 ? str.ToLowerInvariant() : char.ToLowerInvariant(str[0]) + str.Substring(1);
         }
+        public static string GetHashString(this string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in GetHash(s.ToLower().Trim()))
+                sb.Append(b.ToString("X2"));
+
+            return sb.ToString();
+        }
+
+        private static byte[] GetHash(string inputString)
+        {
+            using (HashAlgorithm algorithm = SHA256.Create())
+                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+        }
     }
 
 }
